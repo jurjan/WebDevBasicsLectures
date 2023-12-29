8a200ca4475fa494d10ffcb43e3df5a4d9877f90
jurjan <jurate.janutenaite@gmail.com>
2023-12-29 13:05:35 +0200

    Firebase sample

--- js/index.js
+++ js/index.js
@@ -1,4 +1,90 @@
-const i = 0;
-let otherVar = 2;
+const firebaseURL =
+  "https://webdevbasics-1697b-default-rtdb.europe-west1.firebasedatabase.app/";
+const firebaseConfig = {
+  databaseURL: firebaseURL,
+};
+firebase.initializeApp(firebaseConfig);
+const database = firebase.database();
+const productsRef = database.ref("products:");
 
-console.log();
+// add new
+const newProductRef = productsRef.push();
+newProductRef.set({
+  description: "any-data",
+  in_stock: "any-data",
+  title: "any-data",
+});
+
+console.log(newProductRef.getKey());
+
+//get
+productsRef.on("value", (snapshot) => {
+  const data = snapshot.val();
+  for (const key in data) {
+    const value = data[key];
+    console.log(key);
+    console.log(value);
+  }
+});
+
+//update
+const specificProduct = database.ref("products:/lavender");
+specificProduct.update({ description: "this is updated" });
+
+//delete
+database.ref(`products:/${newProductRef.getKey()}`).remove();
+
+const fetchProducts = fetch(firebaseURL);
+console.log(fetchProducts);
+
+async function firebaseThroughApi() {
+  async function getData() {
+    console.log("getting data through fetch");
+    const fetchProducts = await fetch(`${firebaseURL}/products:.json`);
+    console.log(await fetchProducts.json());
+  }
+
+  getData();
+
+  async function updateData() {
+    console.log("updating data through fetch");
+    const fetchProducts = await fetch(
+      `${firebaseURL}/products:/lavender.json`,
+      {
+        method: "PUT",
+        body: JSON.stringify({
+          description: "this is updated again",
+        }),
+      }
+    );
+    console.log(await fetchProducts.json());
+  }
+
+  updateData();
+
+  async function addData() {
+    console.log("adding data through fetch");
+    const fetchProducts = await fetch(`${firebaseURL}/products:.json`, {
+      method: "POST",
+      body: JSON.stringify({
+        description: "this is updated again",
+      }),
+    });
+    const data = await fetchProducts.json();
+    return data.name;
+  }
+
+  const newId = await addData();
+
+  async function deleteData(id) {
+    console.log("deleting data through fetch");
+    const fetchProducts = await fetch(`${firebaseURL}/products:/${id}.json`, {
+      method: "DELETE",
+    });
+    console.log(fetchProducts);
+  }
+
+  deleteData(newId);
+}
+
+firebaseThroughApi();
